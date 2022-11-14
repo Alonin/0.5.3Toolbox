0b6121183f4eefd2c35a3ff60b85f63751dd49bb
Tim-Couch <timothy.couch2@gmail.com>
2022-11-14 00:44:47 -0600

    add teleport hack

--- 5-3toolbox/Form1.Designer.cs
+++ 5-3toolbox/Form1.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.BGWorker = new System.ComponentModel.BackgroundWorker();
-            this.label1 = new System.Windows.Forms.Label();
             this.ProcOpenLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.coordXBox = new System.Windows.Forms.TextBox();
+            this.teleportButton = new System.Windows.Forms.Button();
+            this.coordYBox = new System.Windows.Forms.TextBox();
+            this.coordZBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BGWorker
@@ -44,66 +46,87 @@
             this.BGWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BGWorker_ProgressChanged);
             this.BGWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BGWorker_RunWorkerCompleted);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Process:";
-            // 
             // ProcOpenLabel
             // 
             this.ProcOpenLabel.AutoSize = true;
-            this.ProcOpenLabel.Location = new System.Drawing.Point(76, 21);
+            this.ProcOpenLabel.Location = new System.Drawing.Point(681, 426);
             this.ProcOpenLabel.Name = "ProcOpenLabel";
-            this.ProcOpenLabel.Size = new System.Drawing.Size(29, 15);
+            this.ProcOpenLabel.Size = new System.Drawing.Size(107, 15);
             this.ProcOpenLabel.TabIndex = 1;
-            this.ProcOpenLabel.Text = "N/A";
+            this.ProcOpenLabel.Text = "Process Not Found";
+            this.ProcOpenLabel.TextChanged += new System.EventHandler(this.ProcOpenLabel_TextChanged);
             // 
-            // textBox1
+            // coordXBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(61, 221);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 2;
+            this.coordXBox.Location = new System.Drawing.Point(32, 12);
+            this.coordXBox.Name = "coordXBox";
+            this.coordXBox.Size = new System.Drawing.Size(100, 23);
+            this.coordXBox.TabIndex = 2;
             // 
-            // button1
+            // teleportButton
             // 
-            this.button1.Location = new System.Drawing.Point(192, 266);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.teleportButton.Location = new System.Drawing.Point(32, 99);
+            this.teleportButton.Name = "teleportButton";
+            this.teleportButton.Size = new System.Drawing.Size(100, 23);
+            this.teleportButton.TabIndex = 3;
+            this.teleportButton.Text = "Teleport!";
+            this.teleportButton.UseVisualStyleBackColor = true;
+            this.teleportButton.Click += new System.EventHandler(this.teleportButton_Click);
             // 
-            // textBox2
+            // coordYBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(167, 221);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 4;
+            this.coordYBox.Location = new System.Drawing.Point(32, 41);
+            this.coordYBox.Name = "coordYBox";
+            this.coordYBox.Size = new System.Drawing.Size(100, 23);
+            this.coordYBox.TabIndex = 4;
             // 
-            // textBox3
+            // coordZBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(273, 221);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
-            this.textBox3.TabIndex = 5;
+            this.coordZBox.Location = new System.Drawing.Point(32, 70);
+            this.coordZBox.Name = "coordZBox";
+            this.coordZBox.Size = new System.Drawing.Size(100, 23);
+            this.coordZBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Z";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.ProcOpenLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.coordZBox);
+            this.Controls.Add(this.coordYBox);
+            this.Controls.Add(this.teleportButton);
+            this.Controls.Add(this.coordXBox);
+            this.Controls.Add(this.ProcOpenLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Shown += new System.EventHandler(this.Form1_Shown);
@@ -115,11 +138,13 @@
         #endregion
 
         private System.ComponentModel.BackgroundWorker BGWorker;
-        private Label label1;
         private Label ProcOpenLabel;
-        private TextBox textBox1;
-        private Button button1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        private TextBox coordXBox;
+        private Button teleportButton;
+        private TextBox coordYBox;
+        private TextBox coordZBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- 5-3toolbox/Form1.cs
+++ 5-3toolbox/Form1.cs
@@ -8,6 +8,7 @@ namespace _5_3toolbox
         public Form1()
         {
             InitializeComponent();
+         
         }
         bool ProcOpen = false;
 
@@ -23,19 +24,25 @@ namespace _5_3toolbox
        
                 Thread.Sleep(1000);
                 BGWorker.ReportProgress(0);
+           
+            
             
         }
         private void Form1_Shown(object sender, EventArgs e)
         {
             BGWorker.RunWorkerAsync();
-
+           
         }
 
         private void BGWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
             if (ProcOpen)
+            {
                 ProcOpenLabel.Text = "WoW Process Found!";
          
+              
+            }
+
         }
 
         private void BGWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
@@ -43,22 +50,22 @@ namespace _5_3toolbox
             BGWorker.RunWorkerAsync();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private void teleportButton_Click(object sender, EventArgs e)
         {
-            float coordinatesbase = m.ReadFloat("base+00CB1FF8,8,198,A0,50");
-            float coordinatesbase2 = m.ReadFloat("base+009C754C,C,40,54");
-            float coordinatesbase3 = m.ReadFloat("base+00A2EB50,1C,34,58");
+            m.WriteMemory("base+00CB1FF8,8,198,A0,50", "float", coordXBox.Text);
+            m.WriteMemory("base+009C754C,C,40,54", "float", coordYBox.Text);
+            m.WriteMemory("base+00A2EB50,1C,34,58", "float", coordZBox.Text);
+        }
 
-
-
-
-            //  float test = coordinatesbase2 + 0x04;
-            MessageBox.Show(coordinatesbase.ToString());
-            MessageBox.Show(coordinatesbase2.ToString());
-            MessageBox.Show(coordinatesbase3.ToString());
-            //m.WriteMemory("coordinatesbase", "float", textBox1.Text);
-            //m.WriteMemory("coordinatesbase+4", "float", textBox2.Text);
-            //m.WriteMemory("coordinatesbase+8" , "float", textBox3.Text);
+        private void ProcOpenLabel_TextChanged(object sender, EventArgs e)
+        {
+            float CoordinateX = m.ReadFloat("base+00CB1FF8,8,198,A0,50");
+            float CoordinateY = m.ReadFloat("base+009C754C,C,40,54");
+            float CoordinateZ = m.ReadFloat("base+00A2EB50,1C,34,58");
+            coordXBox.Text = CoordinateX.ToString();
+            coordYBox.Text = CoordinateY.ToString();
+            coordZBox.Text = CoordinateZ.ToString();
         }
     }
 }
